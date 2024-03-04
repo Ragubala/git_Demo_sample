9aa50417be92db8101bcbe267f285e55ee0a3611
Ragubala <161830867+Ragubala@users.noreply.github.com>
2024-03-04 21:17:43 +0530

    Add files via upload
    just a code upload an github

---
+++ Git_Demo.java
@@ -0,0 +1,10 @@
+package org.git_Demo_sample;
+
+public class Git_Demo {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello_World");
+		System.out.println("\nIm bala from India");
+	}
+
+}
